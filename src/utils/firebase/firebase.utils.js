f48d625d2af0404f2ff811f916624276e93e519b
Calvin Kim <calvinkim@Calvins-Air.lan>
2022-05-26 18:02:18 -0700

    added sign up form

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
-  signInWithRedirect,
+  // signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
