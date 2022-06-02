44ae661511e97add44d30448a0f233476dfea3c4
Calvin Kim <calvinkim@Calvins-Air.lan>
2022-06-02 12:31:51 -0700

    updated checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,31 +1,40 @@
 import { useContext } from "react"
+import CheckoutItem from "../../components/checkout-item/checkout-item.component"
 import { CartContext } from "../../contexts/cart.context"
 import "./checkout.styles.scss"
 
 const Checkout = () => {
 
-    const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext)
+    const { cartItems, cartTotal } = useContext(CartContext)
 
     return (
-        <div>
-            <h1>checkout</h1>
-            <div>
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
                 {
                     cartItems.map((cartItem) => {
-                        const {id, name, quantity} = cartItem
                         return (
-                        <div key={id}>
-                            <h2>{name}</h2>
-                            <span>{quantity}</span>
-                            <br />
-                            <span onClick={() => removeItemFromCart(cartItem)}>minus</span>
-                            <span onClick={() => addItemToCart(cartItem)}>add</span>
-                        </div>
+                        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                         )
                     })
                 }
+                <span className="total">Total: ${cartTotal}</span>
             </div>
-        </div>
     )
 }
 
